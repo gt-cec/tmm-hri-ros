f81a22a0848b80b76e68059f9bb85ed39e8adbd3
Jack Kolb <jack@jackkolb.com>
2025-04-18 17:16:22 -0400

    started removing detection from ros detect, added mental model class from tmm-hri

--- ros_detect_segment.py
+++ ros_detect_segment.py
@@ -9,9 +9,7 @@ import time
 import numpy as np
 from std_msgs.msg import Int32MultiArray
 
-import cec_detect
-import cec_pose
-import cec_dsg
+import mental_model
 
 class WebcamObjectSegmentor:
     def __init__(self):
@@ -24,13 +22,7 @@ class WebcamObjectSegmentor:
         self.bridge = CvBridge()
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-        # Load Owlv2
-        self.owl_processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
-        self.owl_model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32").to(self.device)
-
-        # Load SAMv2
-        self.sam_processor = SamProcessor.from_pretrained("facebook/sam-vit-huge")
-        self.sam_model = SamModel.from_pretrained("facebook/sam-vit-huge").to(self.device)
+        self.mental_model = mental_model.MentalModel()
 
         self.last_saved_time = time.time()
 
@@ -77,7 +69,6 @@ class WebcamObjectSegmentor:
             #     for mask in masks:
             #         segmented_image[mask > 0] = [0, 255, 0]  # Apply green overlay for segmentation
 
-            pose_detector = cec_pose.PoseDetector()
             depth_map = np.zeros((cv_image.shape[0], cv_image.shape[1]))
             robot_pose = [np.array([0, 0]), np.array([1, 0])]
 
@@ -89,56 +80,12 @@ class WebcamObjectSegmentor:
                 "box": box_coords
                 })
 
-
-            pred_person_loc, predicted_heading, other_data = pose_detector.get_heading_of_person(
-            rgb=cv_image,
-            depth_map=depth_map,
-            detected_humans=detected_humans,
-            robot_pose=robot_pose
-        )
-
-            # for box, mask in zip(boxes, masks):
-            #     # Publish bounding box
-            #     bbox_msg = Int32MultiArray(data=box)  # box = [x_min, y_min, x_max, y_max]
-            #     self.box_pub.publish(bbox_msg)
-
-            #     # Publish segmentation mask
-            #     mask = (mask > 0).astype(np.uint8) * 255  # Binary mask
-            #     mask_msg = self.bridge.cv2_to_imgmsg(mask, encoding="mono8")
-            #     self.mask_pub.publish(mask_msg)
-
-            
-
-            # for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-            #     box_int = [int(i) for i in box]
-
-            #     sam_inputs = self.sam_processor(images=rgb_image, input_boxes=[box_int], return_tensors="pt").to(self.device)
-            #     with torch.no_grad():
-            #         sam_outputs = self.sam_model(**sam_inputs)
-            #         mask = sam_outputs.pred_masks.squeeze(0).cpu().numpy().astype(np.uint8) * 255
-
-            #     mask_msg = self.bridge.cv2_to_imgmsg(mask, encoding="mono8")
-                
-                # # Check if 5 seconds have passed since the last saved image
-                # current_time = time.time()
-                # if current_time - self.last_saved_time >= 5:
-                #     # Save the segmented image to disk
-                #     output_dir = "output_images"
-                #     if not os.path.exists(output_dir):
-                #         os.makedirs(output_dir)  # Create directory if it doesn't exist
-
-                #     image_filename = f"{output_dir}/segmented_image_{int(current_time)}.jpg"
-                #     cv2.imwrite(image_filename, segmented_image)
-                #     rospy.loginfo(f"Image saved to: {image_filename}")
-
-                #     # Update the last saved time
-                #     self.last_saved_time = current_time
-
-                # # Convert the segmented image back to ROS Image message
-                # segmented_image_msg = self.bridge.cv2_to_imgmsg(segmented_image, encoding="bgr8")
-
-                # # Publish the segmented image with detections
-                # self.image_pub.publish(segmented_image_msg)
+            pred_person_loc, predicted_heading, other_data = self.pose_detector.get_heading_of_person(
+                rgb=cv_image,
+                depth_map=depth_map,
+                detected_humans=detected_humans,
+                robot_pose=robot_pose
+            )
 
         except Exception as e:
             rospy.logerr(f"Error processing image: {e}")
