3f25f35f27ca0bc73a24cb47d769181895c429b4
Aditya <garg@gatech.edu>
2025-04-18 17:10:02 -0400

    init commit

--- ros_detect_segment.py
+++ ros_detect_segment.py
@@ -7,12 +7,17 @@ import cv2
 import os
 import time
 import numpy as np
+from std_msgs.msg import Int32MultiArray
+import cec_pose
+
 
 class WebcamObjectSegmentor:
     def __init__(self):
         rospy.init_node('webcam_object_segmentor', anonymous=True)
         self.image_sub = rospy.Subscriber('webcam_image', Image, self.image_callback)
         self.image_pub = rospy.Publisher('webcam_segmented_objects', Image, queue_size=10)
+        self.box_pub = rospy.Publisher('/human_boxes', Int32MultiArray, queue_size=10)
+        self.mask_pub = rospy.Publisher('/human_masks', Image, queue_size=10)
 
         self.bridge = CvBridge()
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -65,31 +70,73 @@ class WebcamObjectSegmentor:
                         mask = sam_outputs.pred_masks.squeeze(0).cpu().numpy()
                         masks.append(mask)
 
-                # Overlay the masks on the original image
-                segmented_image = cv_image.copy()
-                for mask in masks:
-                    segmented_image[mask > 0] = [0, 255, 0]  # Apply green overlay for segmentation
+            #     # Overlay the masks on the original image
+            #     segmented_image = cv_image.copy()
+            #     for mask in masks:
+            #         segmented_image[mask > 0] = [0, 255, 0]  # Apply green overlay for segmentation
 
-                # Check if 5 seconds have passed since the last saved image
-                current_time = time.time()
-                if current_time - self.last_saved_time >= 5:
-                    # Save the segmented image to disk
-                    output_dir = "output_images"
-                    if not os.path.exists(output_dir):
-                        os.makedirs(output_dir)  # Create directory if it doesn't exist
+            pose_detector = cec_pose.PoseDetector()
+            depth_map = np.zeros((cv_image.shape[0], cv_image.shape[1]))
+            robot_pose = [np.array([0, 0]), np.array([1, 0])]
 
-                    image_filename = f"{output_dir}/segmented_image_{int(current_time)}.jpg"
-                    cv2.imwrite(image_filename, segmented_image)
-                    rospy.loginfo(f"Image saved to: {image_filename}")
+            detected_humans = []
+            for box, mask in zip(boxes, masks):
+                box_coords = [[box[0], box[1]], [box[2], box[3]]]
+                detected_humans.append({
+                "seg mask": mask,
+                "box": box_coords
+                })
 
-                    # Update the last saved time
-                    self.last_saved_time = current_time
 
-                # Convert the segmented image back to ROS Image message
-                segmented_image_msg = self.bridge.cv2_to_imgmsg(segmented_image, encoding="bgr8")
+            pred_person_loc, predicted_heading, other_data = pose_detector.get_heading_of_person(
+            rgb=cv_image,
+            depth_map=depth_map,
+            detected_humans=detected_humans,
+            robot_pose=robot_pose
+        )
 
-                # Publish the segmented image with detections
-                self.image_pub.publish(segmented_image_msg)
+            # for box, mask in zip(boxes, masks):
+            #     # Publish bounding box
+            #     bbox_msg = Int32MultiArray(data=box)  # box = [x_min, y_min, x_max, y_max]
+            #     self.box_pub.publish(bbox_msg)
+
+            #     # Publish segmentation mask
+            #     mask = (mask > 0).astype(np.uint8) * 255  # Binary mask
+            #     mask_msg = self.bridge.cv2_to_imgmsg(mask, encoding="mono8")
+            #     self.mask_pub.publish(mask_msg)
+
+            
+
+            # for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+            #     box_int = [int(i) for i in box]
+
+            #     sam_inputs = self.sam_processor(images=rgb_image, input_boxes=[box_int], return_tensors="pt").to(self.device)
+            #     with torch.no_grad():
+            #         sam_outputs = self.sam_model(**sam_inputs)
+            #         mask = sam_outputs.pred_masks.squeeze(0).cpu().numpy().astype(np.uint8) * 255
+
+            #     mask_msg = self.bridge.cv2_to_imgmsg(mask, encoding="mono8")
+                
+                # # Check if 5 seconds have passed since the last saved image
+                # current_time = time.time()
+                # if current_time - self.last_saved_time >= 5:
+                #     # Save the segmented image to disk
+                #     output_dir = "output_images"
+                #     if not os.path.exists(output_dir):
+                #         os.makedirs(output_dir)  # Create directory if it doesn't exist
+
+                #     image_filename = f"{output_dir}/segmented_image_{int(current_time)}.jpg"
+                #     cv2.imwrite(image_filename, segmented_image)
+                #     rospy.loginfo(f"Image saved to: {image_filename}")
+
+                #     # Update the last saved time
+                #     self.last_saved_time = current_time
+
+                # # Convert the segmented image back to ROS Image message
+                # segmented_image_msg = self.bridge.cv2_to_imgmsg(segmented_image, encoding="bgr8")
+
+                # # Publish the segmented image with detections
+                # self.image_pub.publish(segmented_image_msg)
 
         except Exception as e:
             rospy.logerr(f"Error processing image: {e}")
